cb8ba75ba2b5f5abd20d1277db0808bcb4055022
Roger Xu <95652473+RogerHXu@users.noreply.github.com>
2023-10-23 12:29:13 -0700

    wip

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from '../app/components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CompanyComponent } from './components/company/company.component';
+
+const routes: Routes = [
+ // { path: "", component: LoginComponent },
+  { path: "", component: CompanyComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
