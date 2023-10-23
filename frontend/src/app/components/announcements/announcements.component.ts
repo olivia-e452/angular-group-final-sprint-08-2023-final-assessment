2bb0c31bfb2f54d630877ae53e7c6b1f7e14c93d
Kurt Gray <kurtgray@gmail.com>
2023-10-23 15:12:30 -0700

    announcements shell

--- frontend/src/app/components/announcements/announcements.component.ts
+++ frontend/src/app/components/announcements/announcements.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+type Announcement = {
+  author: string;
+  date: string;
+  content: string;
+}
+
+type User = {
+  name: string;
+  email: string;
+  password: string;
+  company: string;
+  admin: boolean;
+}
+
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.component.html',
   styleUrls: ['./announcements.component.css']
 })
 export class AnnouncementsComponent {
+
+    user: User | undefined;
+    announcementsToDisplay: Announcement[] | undefined;
+    announcementToCreate: Announcement | undefined;
+    modalOpen = false;
+  
+    constructor() { }
+  
+    ngOnInit(): void {
+      // retrieve a user from somwhere
+      this.user = JSON.parse(localStorage.getItem('userData') || '{}');
+      this.getAnnouncements();
+    }
+  
+    getAnnouncements(): void {
+      const company = localStorage.getItem('company');
+      // a service like the one provided in the Spotify project woudld be nice
+      // fetchFromCompany('GET', company, 'announcements')
+    }
+  
+    handleNewAnnouncement(): void {
+      const company = localStorage.getItem('company');
+      // fetchFromCompany('POST', company, 'announcements', { announcement: this.announcementToCreate })
+    }
+  
+    openModal(): void {
+      this.modalOpen = true;
+    }
+  
+    closeModal(): void {
+      this.modalOpen = false;
+    }
 
 }
