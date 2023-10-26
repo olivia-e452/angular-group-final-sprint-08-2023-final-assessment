f31f68340dfe79575ad4d7c417903c79a0c00bfd
Kurt Gray <kurtgray@gmail.com>
2023-10-26 07:12:29 -0700

    team card styles

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -9,15 +9,25 @@ import fetchFromAPI from 'src/services/api';
 })
 export class AuthService {
 
+<<<<<<< Updated upstream
   private tokenKey = 'user_logged_in';
   //redirectUrl: string | null = '';
   private cred_Status!: Observable<boolean | null>;
+=======
+  private tokenKey = 'token';
+  private tokenExpirationKey = 'token_expiration'
+  private EXPIRATION_TIME = 1000 * 60 * 60 // 1 hour
+
+  redirectUrl: string | null = '';
+  credentials!: Credentials;
+>>>>>>> Stashed changes
 
   constructor(private http: HttpClient, private router: Router) {
   }
 
   login(username: string, password: string) {
     const payload = { "username": username, "password": password }
+<<<<<<< Updated upstream
     let headers = { 'Content-Type': 'application/json' }
     return this.http.post('http://localhost:8080/users/validate', payload, { headers })
       /*.subscribe({
@@ -32,16 +42,41 @@ export class AuthService {
         //console.log(localStorage.getItem('user')); 
         return user;
       }))
+=======
+    let headers =  { 'Content-Type': 'application/json' }
+    console.log(payload)
+    this.http.post('http://localhost:8080/users/validate', payload, {headers}).subscribe(
+      () => {
+        const expiresAt = new Date().getTime() + this.EXPIRATION_TIME;
+        localStorage.setItem(this.tokenExpirationKey, expiresAt.toString())
+        localStorage.setItem(this.tokenKey, "login_token")
+
+        this.router.navigate([this.redirectUrl])
+      });
+    //this.tryLogin(payload);
+  }
+
+  async tryLogin(item: any) {
+    const response = fetchFromAPI("POST", 'users/validate', item);
+>>>>>>> Stashed changes
   }
 
   logout() {
+    localStorage.removeItem(this.tokenExpirationKey);
     localStorage.removeItem(this.tokenKey);
     this.router.navigate(['']);
   }
 
   public isLoggedIn(): boolean {
     let token = localStorage.getItem(this.tokenKey);
-    return token != null && token.length > 0;
+    let expiration = localStorage.getItem(this.tokenExpirationKey);
+    
+    if (expiration && Date.now() > parseInt(expiration)) {
+      this.logout();
+      return false;
+    }
+    
+    return token != null && token.length > 0 
   }
 
   public getToken(): string | null {
