c38907d9c89ff632eefaf7e4c1462d5ba4ea8c14
smalhadid <smalhadid@gmail.com>
2023-10-26 14:36:45 -0400

    added separation of worker and admin logic/page

--- frontend/src/app/components/teams/teams.component.ts
+++ frontend/src/app/components/teams/teams.component.ts
@@ -36,21 +36,30 @@ export class TeamsComponent {
 
   userData: any = {};
   teamData: Team[] | undefined;
+  isUserAdmin: boolean = false;
 
   constructor(private userService : UserService) { }
 
   showModal: boolean = false;
 
   async ngOnInit(): Promise<void> {
-    await this.fetchTeams();
+    if (this.userService.user.isAdmin) {
+      this.isUserAdmin = true;
+      await this.fetchTeams();
+    }
+
+    if (!this.userService.user.isAdmin) {
+      await this.fetchWorkerTeams();
+    }
     console.log(this.teamData);
     // Fetch the number of projects for each team and update teamData
     if (this.teamData) {
       for (const team of this.teamData) {
-        team.numberOfProjects = await this.getNumberOfProjects(6, team.id);
+        team.numberOfProjects = await this.getNumberOfProjects(this.userService.companyID, team.id);
       }
     }
     console.log(this.teamData);
+    console.log(this.userService.user);
   }
   async getNumberOfProjects(
     companyId: number,
@@ -87,4 +96,18 @@ export class TeamsComponent {
       });;
     }
   }
+
+  async fetchWorkerTeams() {
+    this.teamData = await fetchFromAPI("GET", "company/" + this.userService.companyID + "/teams");
+    if (this.teamData !== undefined) {
+      this.teamData = this.teamData.filter(team =>
+        team.teammates.some(teammate => teammate.id === this.userService.user.id)
+      );
+
+      this.teamData.sort((a, b) => {
+        return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0
+      });;
+    }
+  }
+
 }
